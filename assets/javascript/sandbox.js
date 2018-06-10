0ab42e39511a1a703e39aa7ad8aaf1be1a268880
Carlos <cherr004@fiu.edu>
2018-06-10 11:51:46 -0400

    broke out the random crystal numbers to separate functions so that they all had separate randum number values that stayed for the entire game. Also added css and fixed the issue with the win/loss count not adding to the  div. Still need to fix issue with game not resetting properly.

--- assets/javascript/sandbox.js
+++ assets/javascript/sandbox.js
@@ -2,69 +2,60 @@
 $(document).ready(function () {
 
     //Variables we will use
-    var randNumComp = 0;
-    var randNumCrystal = 0;
     var totalScore = 0;
     var losses = "";
     var wins = "";
 
     // function to create the random number (19-120) that will appear in the randNum div
-    var randNum = Math.floor(Math.random() * 120) + 1;
+    var randNumComp = 19 + (Math.floor(Math.random() * 111) + 1);
 
     //Setting function for the random number for the crystals (1-12)
-    var randNumCrystal = Math.floor(Math.random() * 12) + 1;
-
-    //functions that puts the amount of wins/losses tallied into the div for win/losses after the text that was created in the html
-    //this is where connect to HTML to fill in visual data for user 
-
-    var winsTallied = function () {
-        document.querySelector("#wins").innerHTML = "Wins:" + wins;
-    };
-
-    var lossesTallied = function () {
-        document.querySelector("#losses").innerhtml = "Losses:" + losses;
-    };
+    var randNumCrystalRed = Math.floor(Math.random() * 12) + 1;
+    var randNumCrystalBlue = Math.floor(Math.random() * 12) + 1;
+    var randNumCrystalYellow = Math.floor(Math.random() * 12) +1;
+    var randNumCrystalGreen = Math.floor(Math.random() * 12) + 1;
 
     //function called when we reset everything after win or lose
     function reset() {
-        randNumComp = "";
-        randNumCrystal = "";
-        totalScore = "";
+        randNumCrystalRed = "";
+        randNumCrystalBlue = "";
+        randNumCrystalYellow = "";
+        randNumCrystalGreen = "";
+        totalScore = 0;
     };
-
-    // Append the randNum results to the randNum div
-    $("#randNum").text(randNum);
 
     //Setting up a function to reset the game and give an alert if the player wins, add the current score, and reset the game
     function win() {
         alert("You win smarty pants");
         wins++;
-        $("#wins").append(wins)
+        $("#wins").text("Wins:" + wins);
         reset();
     }
     //Setting up a function to reset the game and give an alert if the player losses
     function lose() {
         alert("You lose dumb dumb!");
         losses++;
-        $("#losses").append(losses)
+        $("#losses").text("Losses:" + losses);
         reset();
     };
 
     // function for if else statement stating that if the total score after the button click is = to the generated ranom number you win, if it's greater than the random number, you lose
     function winLoss() {
-        if (totalScore == randNumComp) {
+        if (totalScore === randNumComp) {
             win();
         }
         else if (totalScore > randNumComp) {
             lose();
         }
     };
+    // Insert (like Appending) the randNum to the randNum div
+    $("#randNum").text(randNumComp);
 
     // .on('click to add a listen event for a click) - Then function to create the random number (1-12) when clicking the crystal
     $("#redButton").on("click", function () {
 
         //Adding the random number from the crystal click to the current score from the play (sum of all clicks)
-        totalScore = totalScore + randNumCrystal;
+        totalScore = totalScore + randNumCrystalRed;
 
         //console.log to check if it works
         console.log("totalScore=" + totalScore);
@@ -78,7 +69,7 @@ $(document).ready(function () {
 
     //Blue button function
     $("#blueButton").on("click", function () {
-        totalScore = totalScore + randNumCrystal;
+        totalScore = totalScore + randNumCrystalBlue;
         console.log("totalScore=" + totalScore);
         $("#randNumAgg").text(totalScore);
         winLoss();
@@ -86,7 +77,7 @@ $(document).ready(function () {
 
     //Yellow button functionality code
     $("#yellowButton").on("click", function () {
-        totalScore = totalScore + randNumCrystal;
+        totalScore = totalScore + randNumCrystalYellow;
         console.log("totalScore=" + totalScore);
         $("#randNumAgg").text(totalScore);
         winLoss();
@@ -94,8 +85,8 @@ $(document).ready(function () {
 
     //Green Button functionality code
     $("#greenButton").on("click", function () {
-        totalScore = totalScore + randNumCrystal;
-        console.log("playerScoreTotal=" + userNumAgg);
+        totalScore = totalScore + randNumCrystalGreen;
+        console.log("totalScore=" + totalScore);
         $("#randNumAgg").text(totalScore);
         winLoss();
     });
